c700340e79c082d8860c3280dc602eaa95039443
unicodeveloper <prosperotemuyiwa@gmail.com>
2016-07-04 09:33:41 +0100

    Update properties

--- App.js
+++ App.js
@@ -1,50 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      red: 0,
-      green: 0,
-      blue: 0
-    }
-    this.update = this.update.bind(this)
-  }
-  update(e){
-    this.setState({
-      red: ReactDOM.findDOMNode(this.refs.red).value,
-      green: ReactDOM.findDOMNode(this.refs.green).value,
-      blue: ReactDOM.findDOMNode(this.refs.blue).value
-    })
-  }
   render() {
-    return (
-      <div>
-        <Slider ref="red" update={this.update} />
-        {this.state.red}
-        <br/>
-        <Slider ref="green" update={this.update} />
-        {this.state.green}
-        <br/>
-        <Slider ref="blue" update={this.update} />
-        {this.state.blue}
-        <br />
-      </div>
-    );
+    return <Button>I <Heart/> React</Button>
   }
 }
 
-class Slider extends React.Component {
-
+class Button extends React.Component {
   render() {
-    return (
-        <input type="range"
-          min="0"
-          max="255"
-          onChange={this.props.update} />
-    );
+    return <button>{this.props.children}</button>
   }
 
 }
+
+const Heart = () => <span className="glyphicon glyphicon-heart"></span>
+
+export default App;
 
 ReactDOM.render(<App/>, document.getElementById('app'))
