14514aef7c66e89eec41bf7670bea54f3f03a01e
unicodeveloper <prosperotemuyiwa@gmail.com>
2016-07-04 11:33:36 +0100

    Add mounting usage

--- App.js
+++ App.js
@@ -12,17 +12,20 @@ class App extends React.Component {
     })
   }
   componentWillMount(){
-    console.log("mounting");
+    this.setState({ m: 2 });
   }
   render(){
-    console.log('rendering');
-    return <button onClick={this.update}>{this.state.val}</button>
+    return (
+      <button onClick={this.update}>
+      {this.state.val * this.state.m}
+      </button>
+    )
   }
   componentDidMount(){
-    console.log("mounted...")
+    this.inc = setInterval(this.update, 500)
   }
   componentWillUnmount() {
-    console.log("bye!")
+    clearInterval(this.inc)
   }
 }
 
